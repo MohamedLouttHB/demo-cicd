222002c6d4f65ba7cd8f9cbb96d484f771d4dcb2
MohamedLouttHB <m.loutt99@gmail.com>
2024-06-13 12:45:35 +0100

    add-workflows-att-3

--- src/main/java/com/example/demo_cicd/Controllers.java
+++ src/main/java/com/example/demo_cicd/Controllers.java
@@ -13,7 +13,7 @@ public class Controllers {
 
     @GetMapping()
     public ResponseEntity<String> welcome(){
-        return ResponseEntity.ok("Welcome - test deploy-pull&run-locally-dockerImage");
+        return ResponseEntity.ok("Welcome : test deploy-pull&run-locally-dockerImage- Pipeline");
     }
 
 }
